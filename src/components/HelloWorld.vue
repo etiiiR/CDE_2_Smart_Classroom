24700b292c9af889c4273c22c58773fac14554fa
Etienne Roulet <32195170+etiiiR@users.noreply.github.com>
2022-01-19 18:47:38 +0100

    Initialize bug-vite-plugin-vue-type-import

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<script setup lang='ts'>
+import { Props } from '@/types/HelloWorld';
+
+withDefaults(defineProps<Props>(),{
+	msg : 'Hello World'
+})
+</script>
+
+<template>
+	<div>{{msg}}</div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/pages/index.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+	import { useStore } from '@/store';
+	import VitailseLogo from '@/assets/logo.png';
+	useHead({
+		title: 'Vitailse | Opinionated vite starter template',
+	});
+
+	const store = useStore();
+</script>
+
+<template>
+	<div class="grid place-items-center min-h-[80vh]">
+		<div>
+			<hello-world
+				msg="hello bro this is testing for vite plugin vue type imports"
+			/>
+		</div>
+		<router-link
+			:to="{ name: 'other-page' }"
+			class="
+				mt-5
+				text-center
+				hover:text-gray-200
+				dark:hover:text-gray-500
+				hover:underline
+			"
+			>{{ $t('pages.other.menu') }}</router-link
+		>
+	</div>
+</template>
+
+<route lang="yaml">
+name: home
+</route>
