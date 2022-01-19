e90d441d3ada86766026f11eb76d54990da4a1bc
Etienne Roulet <32195170+etiiiR@users.noreply.github.com>
2022-01-19 18:47:39 +0100

    Initialize bug-vue-tsc

---
+++ src/pages/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+	useHead({
+		title: 'Vitailse | Opinionated vite starter template',
+	});
+</script>
+
+<template>
+	<div class="grid place-items-center min-h-[80vh]">
+		<hello-world msg="Hello World" />
+	</div>
+</template>
+
+<route lang="yaml">
+name: home
+</route>
